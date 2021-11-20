5b29f80a968a950869c6857f3df8a40c88aafea9
ashcui <94345234+ashcui@users.noreply.github.com>
2021-11-20 15:32:23 -0800

    1120 - update06

--- js/cursor.js
+++ js/cursor.js
@@ -1,6 +1,6 @@
-var cursor = new Cursor(".on","incursor");
-var cursor1 = new Cursor(".title","incursor");
-var cursor2 = new Cursor("#logo","incursor");
-var cursor3 = new Cursor(".wait","waitcursor");
-var cursor4 = new Cursor(".github-corner","incursor");
-var cursor5 = new Cursor("a","incursor");
+// var cursor = new Cursor(".on","incursor");
+// var cursor1 = new Cursor(".title","incursor");
+// var cursor2 = new Cursor("#logo","incursor");
+// var cursor3 = new Cursor(".wait","waitcursor");
+// var cursor4 = new Cursor(".github-corner","incursor");
+// var cursor5 = new Cursor("a","incursor");
